5f9b10b0c422b705d825549a684a533fd975e6aa
We're all geeks <wereallgeeks@hotmail.com>
2026-09-10 12:16:00 -0400

    ps2demo changes for VIAL compatibility

---
+++ keyboards/ps2demo/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2F, 0x8E, 0x91, 0x92, 0xEE, 0xEE, 0x15, 0xE7}
+
+#define VIAL_INSECURE
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
