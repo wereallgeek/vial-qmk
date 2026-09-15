47ac263b757993c51cc21d5cd6f7f66db369f61a
We're all geeks <wereallgeeks@hotmail.com>
2026-09-14 21:00:48 -0400

    Preparation for C64 layout
    currently a copy of C16 matrix layout. Will alter later.

--- keyboards/commodore/8bit/keymaps/c64-vial/keymap.c
+++ keyboards/commodore/8bit/keymaps/c64-vial/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_c16(
+  [0] = LAYOUT_c64(
     //ESC                                                                                                [--CURSOR---KEYS---ARE---HERE--] INST/DEL
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LEFT, KC_RGHT, KC_UP,   KC_DOWN, KC_BSPC,   KC_F1,
     
